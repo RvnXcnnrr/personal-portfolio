036449a7cc15a9ca8514b62ab48b18783ae06092
Javy Rodillon <reyfoxconner@gmail.com>
2025-08-01 00:01:36 +0800

    fix: switch back to App component from MaintenanceApp in main render

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles/global.css'
 // Switch between MaintenanceApp and App for maintenance mode
-import MaintenanceApp from './MaintenanceApp.jsx'
-// import App from './App.jsx'
+// import MaintenanceApp from './MaintenanceApp.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MaintenanceApp />
+    <App />
   </StrictMode>,
 )
